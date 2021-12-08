f8d35d967dd3cc0f51a0fbd8d4b79504fa727c34
williamfzc <178894043@qq.com>
2021-12-08 08:03:44 +0000

    feat: base method collect

--- sibyl-core/src/main/java/com/williamfzc/sibyl/core/listener/java8/Java8MethodListener.java
+++ sibyl-core/src/main/java/com/williamfzc/sibyl/core/listener/java8/Java8MethodListener.java
@@ -2,50 +2,72 @@ package com.williamfzc.sibyl.core.listener.java8;
 
 import com.williamfzc.sibyl.core.listener.Java8Parser;
 import com.williamfzc.sibyl.core.model.method.Method;
+import com.williamfzc.sibyl.core.model.method.MethodBelonging;
+import com.williamfzc.sibyl.core.model.method.MethodInfo;
 import com.williamfzc.sibyl.core.utils.Log;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Java8MethodListener extends Java8StorableListener {
+    private String curPackage;
+    private final Deque<String> curClassStack = new LinkedList<>();
+
     @Override
     public void enterPackageDeclaration(Java8Parser.PackageDeclarationContext ctx) {
-        // a list, [cases, java8] == cases.java8
-        Log.info("pkg decl: " + ctx.Identifier().toString());
+        String declaredPackage =
+                ctx.Identifier().stream().map(ParseTree::getText).collect(Collectors.joining("."));
+        Log.info("pkg decl: " + declaredPackage);
+        curPackage = declaredPackage;
     }
 
     // use a stack to manage current class
     @Override
     public void enterClassDeclaration(Java8Parser.ClassDeclarationContext ctx) {
-        Log.info("class decl: " + ctx.normalClassDeclaration().Identifier().getText());
+        String declaredClass = ctx.normalClassDeclaration().Identifier().getText();
+        Log.info("class decl: " + declaredClass);
+        curClassStack.push(declaredClass);
     }
 
     @Override
     public void exitClassDeclaration(Java8Parser.ClassDeclarationContext ctx) {
-        Log.info("class decl end: " + ctx.normalClassDeclaration().Identifier().getText());
+        String declaredClass = ctx.normalClassDeclaration().Identifier().getText();
+        Log.info("class decl end: " + declaredClass);
+        curClassStack.pop();
     }
 
     // use a stack to manage current method
     @Override
     public void enterMethodDeclaration(Java8Parser.MethodDeclarationContext ctx) {
-        Log.info("method decl: " + ctx.methodHeader().methodDeclarator().Identifier().getText());
+        String declaredMethod = ctx.methodHeader().methodDeclarator().Identifier().getText();
+        Log.info("method decl: " + declaredMethod);
+        String curClass = curClassStack.peekLast();
         Method m = new Method();
-        m.setId(1L);
 
-        if (this.storage == null) {
-            Log.info("storage null!!1");
-        }
+        MethodInfo info = new MethodInfo();
+        info.setName(declaredMethod);
+
+        MethodBelonging belonging = new MethodBelonging();
+        belonging.setPackageName(curPackage);
+        belonging.setClassName(curClass);
+
+        m.setInfo(info);
+        m.setBelongsTo(belonging);
+
         this.storage.save(m);
     }
 
     @Override
     public void exitMethodDeclaration(Java8Parser.MethodDeclarationContext ctx) {
-        Log.info(
-                "method decl end: " + ctx.methodHeader().methodDeclarator().Identifier().getText());
+        String declaredMethod = ctx.methodHeader().methodDeclarator().Identifier().getText();
+        Log.info("method decl end: " + declaredMethod);
     }
 
     @Override
     public void enterFieldDeclaration(Java8Parser.FieldDeclarationContext ctx) {
-        Log.info(
-                String.format(
-                        "field decl, type: %s, value: %s",
-                        ctx.unannType().getText(), ctx.variableDeclaratorList().getText()));
+        String declaredField = ctx.unannType().getText();
+        String declaredValue = ctx.variableDeclaratorList().getText();
+        Log.info(String.format("field decl, type: %s, value: %s", declaredField, declaredValue));
     }
 }
--- sibyl-core/src/test/java/com/williamfzc/sibyl/core/TestMain.java
+++ sibyl-core/src/test/java/com/williamfzc/sibyl/core/TestMain.java
@@ -5,6 +5,7 @@ import com.williamfzc.sibyl.core.listener.java8.Java8MethodListener;
 import com.williamfzc.sibyl.core.model.method.Method;
 import com.williamfzc.sibyl.core.scanner.NormalScanner;
 import com.williamfzc.sibyl.core.storage.Storage;
+import com.williamfzc.sibyl.core.utils.Log;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -25,8 +26,6 @@ public class TestMain {
         scanner.scanDir(new File(currentRelativePath.toAbsolutePath().toString(), "src"));
 
         System.out.println("method count: " + listener.getStorage().size());
-        listener.getStorage()
-                .getData()
-                .forEach(each -> System.out.println("get method id: " + each.getId()));
+        methodStorage.getData().forEach(each -> Log.info(each.toString()));
     }
 }
