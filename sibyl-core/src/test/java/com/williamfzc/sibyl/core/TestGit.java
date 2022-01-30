287b82624830591f291bc1407f0090000730205d
williamfzc <178894043@qq.com>
2022-01-30 07:43:56 +0000

    chore: ignore git case
    can not run in github action

--- sibyl-core/src/test/java/com/williamfzc/sibyl/core/TestGit.java
+++ sibyl-core/src/test/java/com/williamfzc/sibyl/core/TestGit.java
@@ -6,9 +6,13 @@ import java.io.*;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestGit {
+    // this case can not run in github action
+    // it requires .git dir
+    @Ignore
     @Test
     public void testGitDiff() throws IOException {
         Repository repo = new RepositoryBuilder().findGitDir(Support.getProjectRoot()).build();
