994dfcbaa2d22a86123266a59954caa05431fea2
williamfzc <178894043@qq.com>
2022-02-17 23:41:41 +0800

    feat: script for quick usage

---
+++ scripts/diff.sh
@@ -0,0 +1,27 @@
+set -e
+basepath=$(cd `dirname $0`; pwd)
+
+cd $basepath
+cd ..
+ws=`pwd`
+version=0.3.0
+echo "current workspace: $ws"
+echo "sibyl version: $version"
+
+file_name=sibyl-cli-${version}-jar-with-dependencies.jar
+file_path=${ws}/${file_name}
+if [ ! -f "$file_path" ]; then
+  echo "sibyl cli is not existed"
+  wget https://github.com/williamfzc/sibyl/releases/download/v${version}/${file_name}
+fi
+
+# ready
+before=`git rev-parse HEAD~`
+after=`git rev-parse HEAD`
+echo "before: ${before}"
+echo "after: ${after}"
+
+output=${ws}/diff.json
+java -version
+java -jar "$file_path" diff -i . -o "$output" --before "$before" --after "$after" -t JAVA_8
+echo "diff finished: ${output}"
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,13 +3,14 @@ basepath=$(cd `dirname $0`; pwd)
 
 cd $basepath
 cd ..
-cd ..
 echo `pwd`
 
 rm -rf testRes
 mkdir testRes
 cd testRes
 echo "clone test res from github ..."
+
+# --- java ---
 # 3000+ files
 # too large for github hosted runner?
 #git clone --depth 1 https://github.com/google/guava.git
