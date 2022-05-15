6c297637eb2a139b9235494e68e3abc23f8d20d3
williamfzc <178894043@qq.com>
2022-05-15 19:34:52 +0800

    feat: jqf-fuzz autogen

--- sibyl-ut-generator/src/main/java/com/williamfzc/sibyl/utgen/UtGen.java
+++ sibyl-ut-generator/src/main/java/com/williamfzc/sibyl/utgen/UtGen.java
@@ -1,17 +1,18 @@
 package com.williamfzc.sibyl.utgen;
 
-import com.squareup.javapoet.JavaFile;
-import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
 import com.williamfzc.sibyl.core.api.Sibyl;
 import com.williamfzc.sibyl.core.api.SibylLangType;
 import com.williamfzc.sibyl.core.api.internal.SibylDiff;
 import com.williamfzc.sibyl.core.model.clazz.Clazz;
 import com.williamfzc.sibyl.core.model.diff.DiffResult;
 import com.williamfzc.sibyl.core.model.method.Method;
+import com.williamfzc.sibyl.core.model.method.Parameter;
 import com.williamfzc.sibyl.core.scanner.ScanPolicy;
 import com.williamfzc.sibyl.core.storage.Storage;
 import com.williamfzc.sibyl.core.utils.SibylLog;
+import edu.berkeley.cs.jqf.fuzz.Fuzz;
+import edu.berkeley.cs.jqf.fuzz.JQF;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -20,9 +21,12 @@ import javax.lang.model.element.Modifier;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
-import org.junit.Test;
+import org.junit.runner.RunWith;
 
 public class UtGen {
+    private static final String NAME_METHOD_TARGET_GETTER = "_getUtGenTarget";
+    private static final String NAME_LITERAL_LOCAL_TARGET = "_utGenTarget";
+
     public static Storage<? extends Method> collectMethods(
             File projectDir, String endRev, String startRev)
             throws IOException, InterruptedException {
@@ -62,17 +66,31 @@ public class UtGen {
             // transfer this method
             MethodSpec.Builder builder =
                     MethodSpec.methodBuilder("test_" + method.getInfo().getName())
-                            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                            .addModifiers(Modifier.PUBLIC)
                             .returns(void.class);
-            builder.addAnnotation(Test.class);
+            builder.addAnnotation(Fuzz.class);
 
             // main execution
             try {
                 builder.addCode(
                         String.format(
-                                "%s o = new %s();\n",
+                                "%s %s = %s();\n",
                                 method.getBelongsTo().getClazz().getFullName(),
-                                method.getBelongsTo().getClazz().getFullName()));
+                                NAME_LITERAL_LOCAL_TARGET,
+                                NAME_METHOD_TARGET_GETTER));
+                // execute
+                for (Parameter param : method.getInfo().getParams()) {
+                    builder.addParameter(ClassName.bestGuess(param.getType()), param.getName());
+                }
+                builder.addCode(
+                        String.format(
+                                "%s.%s(%s);\n",
+                                NAME_LITERAL_LOCAL_TARGET,
+                                method.getInfo().getName(),
+                                method.getInfo().getParams().stream()
+                                        .map(Parameter::getName)
+                                        .collect(Collectors.joining(", "))));
+
             } catch (NullPointerException e) {
                 continue;
             }
@@ -85,11 +103,33 @@ public class UtGen {
         return cache.entrySet().stream()
                 .map(
                         item -> {
-                            TypeSpec cur =
+                            TypeSpec.Builder builder =
                                     TypeSpec.classBuilder("Test" + item.getKey().getName())
-                                            .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                                            .addMethods(item.getValue())
-                                            .build();
+                                            .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+                            builder.addMethods(item.getValue());
+                            MethodSpec.Builder getterBuilder =
+                                    MethodSpec.methodBuilder(NAME_METHOD_TARGET_GETTER)
+                                            .addModifiers(Modifier.PRIVATE)
+                                            .returns(
+                                                    ClassName.get(
+                                                            item.getKey()
+                                                                    .getBelongsTo()
+                                                                    .getPkg()
+                                                                    .getName(),
+                                                            item.getKey().getName()));
+                            getterBuilder.addCode(
+                                    String.format("return new %s();\n", item.getKey().getName()));
+                            builder.addMethod(getterBuilder.build());
+                            AnnotationSpec.Builder runWithBuilder =
+                                    AnnotationSpec.builder(RunWith.class)
+                                            .addMember(
+                                                    "value",
+                                                    CodeBlock.builder()
+                                                            .add("$T.class", JQF.class)
+                                                            .build());
+                            builder.addAnnotation(runWithBuilder.build());
+
+                            TypeSpec cur = builder.build();
                             return JavaFile.builder(
                                             item.getKey().getBelongsTo().getPkg().getName(), cur)
                                     .build();
--- sibyl-ut-generator/src/test/java/com/williamfzc/sibyl/utgen/TestApi.java
+++ sibyl-ut-generator/src/test/java/com/williamfzc/sibyl/utgen/TestApi.java
@@ -11,12 +11,18 @@ public class TestApi {
     @Test
     public void ok() throws IOException, InterruptedException {
         Storage<Method> ret = (Storage<Method>) UtGen.collectAllMethods(Support.getProjectRoot());
+        //        Storage<Method> ret =
+        //                (Storage<Method>) UtGen.collectMethods(Support.getProjectRoot(), "HEAD",
+        // "HEAD~~~");
 
         UtGen.methodsToCases(ret)
                 .forEach(
                         each -> {
                             try {
-                                each.writeTo(new File(Support.getProjectRoot(), "testbed"));
+                                each.writeTo(
+                                        new File(
+                                                Support.getProjectRoot(),
+                                                "sibyl-ut-generator/src/test/java1"));
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
