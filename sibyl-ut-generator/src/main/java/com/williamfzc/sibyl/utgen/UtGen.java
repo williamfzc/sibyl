cc2cac60eb50236f0edb9744ac04cb8e0d9fa9d2
williamfzc <178894043@qq.com>
2022-05-15 16:00:21 +0800

    feat(utgen): methods -> cases -> files

--- sibyl-ut-generator/src/main/java/com/williamfzc/sibyl/utgen/UtGen.java
+++ sibyl-ut-generator/src/main/java/com/williamfzc/sibyl/utgen/UtGen.java
@@ -1,9 +1,12 @@
 package com.williamfzc.sibyl.utgen;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import com.williamfzc.sibyl.core.api.Sibyl;
 import com.williamfzc.sibyl.core.api.SibylLangType;
 import com.williamfzc.sibyl.core.api.internal.SibylDiff;
-import com.williamfzc.sibyl.core.model.diff.DiffMethod;
+import com.williamfzc.sibyl.core.model.clazz.Clazz;
 import com.williamfzc.sibyl.core.model.diff.DiffResult;
 import com.williamfzc.sibyl.core.model.method.Method;
 import com.williamfzc.sibyl.core.scanner.ScanPolicy;
@@ -11,15 +14,21 @@ import com.williamfzc.sibyl.core.storage.Storage;
 import com.williamfzc.sibyl.core.utils.SibylLog;
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import javax.lang.model.element.Modifier;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.junit.Test;
 
 public class UtGen {
-    public static void gen(File projectDir) throws IOException, InterruptedException {
+    public static Storage<? extends Method> collectMethods(
+            File projectDir, String endRev, String startRev)
+            throws IOException, InterruptedException {
         Repository repo = new RepositoryBuilder().findGitDir(projectDir).build();
-        ObjectId head = repo.resolve("HEAD");
-        ObjectId headParent = repo.resolve("HEAD~~~~");
+        ObjectId head = repo.resolve(endRev);
+        ObjectId headParent = repo.resolve(startRev);
         SibylLog.info("after: " + head.getName());
         SibylLog.info("before: " + headParent.getName());
 
@@ -37,22 +46,54 @@ public class UtGen {
         Storage<Method> methodStorage =
                 Sibyl.genSnapshotFromDir(projectDir, SibylLangType.JAVA_8, scanPolicy);
         assert methodStorage != null;
-        Storage<DiffMethod> methods = Sibyl.genSnapshotDiff(methodStorage, diffResult);
-        SibylLog.info("diff method count: " + methods.size());
+        return Sibyl.genSnapshotDiff(methodStorage, diffResult);
+    }
 
-        // methods to cases
-        methods.getData()
-                .forEach(
-                        each -> {
-                            try {
-                                SibylLog.info(
-                                        String.format(
-                                                "%s call %s with params %s",
-                                                each.getBelongsTo().getClazz().getFullName(),
-                                                each.getInfo().getName(),
-                                                each.getInfo().getParams()));
-                            } catch (NullPointerException ignored) {
-                            }
-                        });
+    public static Storage<? extends Method> collectAllMethods(File projectDir)
+            throws IOException, InterruptedException {
+        return Sibyl.genSnapshotFromDir(projectDir, SibylLangType.JAVA_8);
+    }
+
+    public static Set<JavaFile> methodsToCases(Storage<? extends Method> methods) {
+        Map<Clazz, List<MethodSpec>> cache = new HashMap<>();
+        for (Method method : methods.getData()) {
+            Clazz clazz = method.getBelongsTo().getClazz();
+
+            // transfer this method
+            MethodSpec.Builder builder =
+                    MethodSpec.methodBuilder("test_" + method.getInfo().getName())
+                            .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                            .returns(void.class);
+            builder.addAnnotation(Test.class);
+
+            // main execution
+            try {
+                builder.addCode(
+                        String.format(
+                                "%s o = new %s();\n",
+                                method.getBelongsTo().getClazz().getFullName(),
+                                method.getBelongsTo().getClazz().getFullName()));
+            } catch (NullPointerException e) {
+                continue;
+            }
+
+            cache.putIfAbsent(clazz, new LinkedList<>());
+            cache.get(clazz).add(builder.build());
+        }
+
+        // create real cases
+        return cache.entrySet().stream()
+                .map(
+                        item -> {
+                            TypeSpec cur =
+                                    TypeSpec.classBuilder("Test" + item.getKey().getName())
+                                            .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                                            .addMethods(item.getValue())
+                                            .build();
+                            return JavaFile.builder(
+                                            item.getKey().getBelongsTo().getPkg().getName(), cur)
+                                    .build();
+                        })
+                .collect(Collectors.toSet());
     }
 }
--- sibyl-ut-generator/src/test/java/com/williamfzc/sibyl/utgen/TestApi.java
+++ sibyl-ut-generator/src/test/java/com/williamfzc/sibyl/utgen/TestApi.java
@@ -1,12 +1,25 @@
 package com.williamfzc.sibyl.utgen;
 
+import com.williamfzc.sibyl.core.model.method.Method;
+import com.williamfzc.sibyl.core.storage.Storage;
 import com.williamfzc.sibyl.test.Support;
+import java.io.File;
 import java.io.IOException;
 import org.junit.Test;
 
 public class TestApi {
     @Test
     public void ok() throws IOException, InterruptedException {
-        UtGen.gen(Support.getProjectRoot());
+        Storage<Method> ret = (Storage<Method>) UtGen.collectAllMethods(Support.getProjectRoot());
+
+        UtGen.methodsToCases(ret)
+                .forEach(
+                        each -> {
+                            try {
+                                each.writeTo(new File(Support.getProjectRoot(), "testbed"));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
     }
 }
