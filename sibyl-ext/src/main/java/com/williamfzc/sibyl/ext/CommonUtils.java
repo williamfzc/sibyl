fac361664bb080d7ff05d64fb6332ace403ff4e1
williamfzc <178894043@qq.com>
2022-07-09 06:13:41 +0000

    fix(ext/spring): wrong package and clazz parser

--- sibyl-ext/src/main/java/com/williamfzc/sibyl/ext/CommonUtils.java
+++ sibyl-ext/src/main/java/com/williamfzc/sibyl/ext/CommonUtils.java
@@ -6,10 +6,16 @@ public class CommonUtils {
     }
 
     public static String fullPath2ClazzName(String fullPath) {
+        if (!fullPath.contains(".")) {
+            return fullPath;
+        }
         return fullPath.substring(fullPath.lastIndexOf('.') + 1);
     }
 
     public static String fullPath2PackageName(String fullPath) {
+        if (!fullPath.contains(".")) {
+            return "";
+        }
         return fullPath.substring(0, fullPath.lastIndexOf('.'));
     }
 }
