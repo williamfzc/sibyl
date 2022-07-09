472002a5eca797c40378eb0c126aa7498ce85546
williamfzc <178894043@qq.com>
2022-07-09 06:15:19 +0000

    fix(ext/spring): wrong annotation

--- sibyl-ext/src/main/java/com/williamfzc/sibyl/ext/spring/exporter/JUnitExporter.java
+++ sibyl-ext/src/main/java/com/williamfzc/sibyl/ext/spring/exporter/JUnitExporter.java
@@ -5,17 +5,19 @@ import com.williamfzc.sibyl.core.model.method.Parameter;
 import com.williamfzc.sibyl.ext.CommonUtils;
 import com.williamfzc.sibyl.ext.spring.model.JUnitCaseFile;
 import com.williamfzc.sibyl.ext.spring.model.ServiceCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import javax.lang.model.element.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import javax.annotation.Resource;
-import javax.lang.model.element.Modifier;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 
 public class JUnitExporter extends BaseExporter {
     // create methods
@@ -28,11 +30,35 @@ public class JUnitExporter extends BaseExporter {
                         .addModifiers(Modifier.PUBLIC)
                         .returns(void.class);
         methodBuilder.addAnnotation(Test.class);
+        methodBuilder.addComment("TODO: This method was auto-generated");
 
+        // params
+        List<Parameter> parameters = serviceCase.getParams();
+        if (null == parameters) {
+            parameters = new ArrayList<>();
+        }
+        for (Parameter eachParam : parameters) {
+            ClassName guessed;
+            try {
+                guessed = ClassName.bestGuess(eachParam.getType());
+            } catch (IllegalArgumentException e) {
+                // guess failed
+                guessed =
+                        ClassName.get(
+                                CommonUtils.fullPath2PackageName(eachParam.getType()),
+                                CommonUtils.fullPath2ClazzName(eachParam.getType()));
+            }
+            methodBuilder.addStatement("$T $N = null", guessed, eachParam.getName());
+        }
+
+        // call
         String paramsStr = "";
-        if (null != serviceCase.getParams()) {
+
+        if (parameters.size() == 1) {
+            paramsStr = parameters.get(0).getName();
+        } else if (parameters.size() > 1) {
             paramsStr =
-                    serviceCase.getParams().stream()
+                    parameters.stream()
                             .filter(Objects::nonNull)
                             .map(Parameter::getName)
                             .collect(Collectors.joining(", "));
@@ -76,6 +102,7 @@ public class JUnitExporter extends BaseExporter {
                                 "value",
                                 CodeBlock.builder().add("$T.class", SpringRunner.class).build());
         clazzBuilder.addAnnotation(runWithBuilder.build());
+        clazzBuilder.addAnnotation(SpringBootTest.class);
 
         TypeSpec cur = clazzBuilder.build();
         return JUnitCaseFile.of(JavaFile.builder(packageName, cur).build());
@@ -113,11 +140,5 @@ public class JUnitExporter extends BaseExporter {
             ret.add(cur);
         }
         return ret;
-    }
-
-    public List<String> cases2JavaFilesRaw(Iterable<ServiceCase> serviceCases) {
-        return cases2JavaFiles(serviceCases).stream()
-                .map(JUnitCaseFile::genValidCaseContent)
-                .collect(Collectors.toList());
     }
 }
